7b5b47e671de63a088d7a0c7e6e7685b5b71e8b4
SaintSergey <44222625+SaintSergey@users.noreply.github.com>
2018-10-17 10:39:04 +0300

    updated my_name

--- homework.py
+++ homework.py
@@ -1,8 +1,8 @@
 import hashlib
 
-my_name = "d.galamaga"
+my_name = "Sergey.Krasilnikov"
 m = hashlib.md5()
 m.update(my_name.encode())
 
 if __name__ == "__main__":
-    print(f"Task completed by {Sergey.Krasilnikov}! md5 is {m.hexdigest()}")
+    print(f"Task completed by {my_name}! md5 is {m.hexdigest()}")
